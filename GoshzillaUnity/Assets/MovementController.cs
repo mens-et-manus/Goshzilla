571ca8c785dde7a1e55b8f3a88cf56d34abcfc6f
Kevin Tan <kangrui10@hotmail.com>
2017-01-13 23:45:51 +0800

    Colliders and non-Kinect movement

--- GoshzillaUnity/Assets/MovementController.cs
+++ GoshzillaUnity/Assets/MovementController.cs
@@ -1,36 +1,128 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MovementController : MonoBehaviour {
 
 	public Component godzilla;
 
-	private int timing;
-	private int timing2;
+    //add end point
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void FixedUpdate () {
-		if(transform.position.y > 0.5){
-			timing += 1;
-			godzilla.transform.localPosition += new Vector3(0.0f, 0.0f, 0.1f);
-	}
-		if(transform.position.y < 0.5){
-			if(timing >= 5){
-				timing2 += 1;
-				if(timing2 < 20){
-				godzilla.transform.localPosition += new Vector3(0.0f, 0.0f, 0.1f);
-				}else{
-					timing2 = 0;
-					timing = 0;
-				}
-			}
-		}
+
+    //private Rigidbody rb;
+    private int buildingCount;
+    private int peopleCount;
+    private int carCount;
+    private int casualtyCount;
+    private Vector3 distance;
+    private Vector3 originalDistance;
+    private int cityElementCount;
+
+    public int speed;
+    public Text buildingsDestroyed;
+    public Text peopleKilled;
+    public Text carsDestroyed;
+    public Text casualty;
+    public Text distanceTravelled;
+    public Text winText;
+    public Text cityElementDestroyed;
+
+    void Start()
+    {
+        buildingCount = 0;
+        peopleCount = 0;
+        carCount = 0;
+        cityElementCount = 0;
+        originalDistance = godzilla.transform.position;
+
+        //rb = GetComponent<Rigidbody>();
+
+        buildingsDestroyed.text = "Buildings Destroyed: 0";
+        peopleKilled.text = "Innocent People Trampled: 0";
+        carsDestroyed.text = "Cars Destroyed: 0";
+        cityElementDestroyed.text = "Miscellaneous Structures Destroyed: 0";
+        casualty.text = "Casualties: 0";
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Building"))
+        {
+            Destroy(other.gameObject);
+            buildingCount += 1;
+            updateScores(1);
+        }
+        else if (other.gameObject.CompareTag("People"))
+        {
+            Destroy(other.gameObject);
+            peopleCount += 1;
+            updateScores(2);
+        }
+        else if (other.gameObject.CompareTag("Car"))
+        {
+            Destroy(other.gameObject);
+            carCount += 1;
+            updateScores(3);
+        }
+        else if (other.gameObject.CompareTag("City Element"))
+        {
+            Destroy(other.gameObject);
+            cityElementCount += 1;
+            updateScores(4);
+        }
+        else if (other.gameObject.CompareTag("End")) //TEMP TAG FOR END POINT
+        {
+            if (casualtyCount <= 500)
+            {
+                winText.text = "You win!";
+            }
+            else
+            {
+                winText.text = "Goshzilla, you lose!";
+            }
+        }
+    }
+    void Update()
+    {
+        distance = godzilla.transform.position - originalDistance;
+        distanceTravelled.text = "Distance Travelled: " + Mathf.Round(distance.sqrMagnitude).ToString();
+    }
+
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+        // rb.AddForce(movement * speed);
+        godzilla.transform.position += movement*Time.deltaTime;
+
+    }
+
+    void updateScores(int type)
+    {
+        switch (type)
+        {
+            case 1:
+                buildingsDestroyed.text = "Buildings Destroyed: " + buildingCount.ToString();
+                break;
+            case 2:
+                peopleKilled.text = "Innocent People Trampled: " + peopleCount.ToString();
+                break;
+            case 3:
+                carsDestroyed.text = "Cars Destroyed: " + carCount.ToString();
+                break;
+            case 4:
+                cityElementDestroyed.text = "Miscellaneous Structures Destroyed: " + cityElementCount.ToString();
+                break;
+
+        }
+        casualtyCount = buildingCount * 100 + peopleCount + carCount * 4;
+        casualty.text = "Casualties: " + casualtyCount.ToString();
+
+    }
 }
 
 	//Simple movement
@@ -53,4 +145,4 @@ public class MovementController : MonoBehaviour {
 		}
 	}
 	*/
-}
+
---
+++ GoshzillaUnity/Assets/Scripts/CollisionScript.cs
@@ -0,0 +1,112 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class CollisionScript : MonoBehaviour {
+
+    //add this script to goshzilla, put some code for movement.
+
+    private Rigidbody rb;
+    private int buildingCount;
+    private int peopleCount;
+    private int carCount;
+    private int casualtyCount;
+    private Vector3 distance;
+    private Vector3 originalDistance;
+
+    public int speed;
+    public Text buildingsDestroyed;
+    public Text peopleKilled;
+    public Text carsDestroyed;
+    public Text casualty;
+    public Text distanceTravelled;
+    public Text winText;
+    
+    void Start()
+    {
+        buildingCount = 0;
+        peopleCount = 0;
+        carCount = 0;
+        originalDistance = transform.position;
+    
+        rb = GetComponent<Rigidbody>();
+
+        buildingsDestroyed.text = "Buildings Destroyed: 0";
+        peopleKilled.text = "Innocent People Trampled: 0";
+        carsDestroyed.text = "Cars Destroyed: 0";
+        casualty.text = "Casualties: 0";
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Building"))
+        {
+            Destroy(other.gameObject);
+            buildingCount += 1;
+            updateScores(1);
+        }
+        else if (other.gameObject.CompareTag("People"))
+        {
+            Destroy(other.gameObject);
+            peopleCount += 1;
+            updateScores(2);
+        }
+        else if (other.gameObject.CompareTag("Car"))
+        {
+            Destroy(other.gameObject);
+            carCount += 1;
+            updateScores(3);
+        }
+        else if (other.gameObject.CompareTag("City Element"))
+        {
+
+        }
+        else if (other.gameObject.CompareTag("Tree")) //TEMP TAG FOR END POINT
+        {
+            if (casualtyCount <= 500)
+            {
+                winText.text = "You win!";
+            }
+            else
+            {
+                winText.text = "Goshzilla, you lose!";
+            }
+        }
+    }
+    void Update()
+    {
+        distance = transform.position - originalDistance;
+        distanceTravelled.text = "Distance Travelled: " + Mathf.Round(distance.sqrMagnitude).ToString();
+    }
+
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+        rb.AddForce(movement * speed);
+
+    }
+
+    void updateScores(int type)
+    {
+        switch (type)
+        {
+            case 1:
+                buildingsDestroyed.text = "Buildings Destroyed: " + buildingCount.ToString();
+                break;
+            case 2:
+                peopleKilled.text = "Innocent People Trampled: " + peopleCount.ToString();
+                break;
+            case 3:
+                carsDestroyed.text = "Cars Destroyed: " + carCount.ToString();
+                break;
+            
+        }
+        casualtyCount = buildingCount * 100 + peopleCount + carCount * 4;
+        casualty.text = "Casualties: " + casualtyCount.ToString();
+
+    }
+}
